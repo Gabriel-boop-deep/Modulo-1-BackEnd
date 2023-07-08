7cda94b98c4970adedb384dca57c766260d43cd2
Gabriel Nunes Barbosa Nogueira <nunesnbnxn@gmail.com>
2023-07-08 07:54:46 -0300

    Atualizando commit

---
+++ calculadora.js
@@ -0,0 +1,11 @@
+let a = 5
+let b = 8
+let soma = a + b
+let subtracao = a - b
+let multiplicacao = a * b
+let divisao = a / b
+
+console.log("A soma dos dois números é : ", soma)
+console.log("A subtração dos dois números é :", subtracao)
+console.log("A multiplicação dos dois números é :", multiplicacao)
+console.log("A divisão dos dois números é :", divisao)
